2921b1fe2d9b47ffa8f932b1f95feb756b82298d
theshy2025 <theshy2025@gmail.com>
2024-04-28 16:22:10 +0800

    step status

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ pub const BUFF_SIZE:usize = 1024*64;
 pub const ATYP_INDEX:usize = 3;
 pub const ATYP_HOST_NAME:u8 = 3;
 
-pub const GATE:u64 = 1;
-pub const DNS:u64 = 2;
+pub const GATE_ID:u64 = 1;
+pub const DNS_ID:u64 = 2;
 
 pub const TCP_LIFE_TIME:i64 = 30;
--- src/ss/line/data.rs
+++
@@ -1,8 +0,0 @@
-use crate::ss::Line;
-
-impl Line {
-    pub fn set_peer_ip(&mut self,s:String) {
-        self.log(format!("peer_ip change from {:?} to {:?}",self.peer_ip,s));
-        self.peer_ip = s;
-    }
-}
